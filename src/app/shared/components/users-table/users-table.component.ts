1b7eb15142e26909fb9230673185b0a199e507ec
Ibrahim Okić <70588174+kaizerpwn@users.noreply.github.com>
2024-06-20 13:24:00 +0200

    Showing users in the data table

--- src/app/shared/components/users-table/users-table.component.ts
+++ src/app/shared/components/users-table/users-table.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { UserService } from '../../../core/services/user.service';
 import { User } from '../../../core/intefaces/user';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-users-table',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './users-table.component.html',
   styleUrl: './users-table.component.css'
 })
